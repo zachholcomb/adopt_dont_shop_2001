ee2c9e832cb7558c671a0fe982a1800cc84a09d5
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-17 16:41:40 -0600

    Add show functionality to display shelter info

---
+++ spec/features/shelters/shelter_id_show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "shelter id page" do
+  it "can display shelter information on id page" do
+    shelter_1 = Shelter.create!(name: "Denver Animal Shelter",
+                               address: "500 Invisible St.",
+                               city: "Denver",
+                               state: "Colorado",
+                               zip: "80201")
+
+  visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_1.address)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to have_content(shelter_1.state)
+    expect(page).to have_content(shelter_1.zip)
+  end
+end
