4b89fa7688b6d01ae70b5e6bb3c14411d186caa6
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-18 09:41:04 -0600

    Add update functionality to shelters show page

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -10,4 +10,34 @@ class SheltersController < ApplicationController
 
   def new
   end
+
+  def create
+    shelter = Shelter.new({
+      name: params[:name],
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip]
+      })
+
+      shelter.save
+      redirect_to '/shelters'
+  end
+
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update({
+      name: params[:name],
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip]
+      })
+      shelter.save
+      redirect_to "/shelters/#{shelter.id}"
+  end
 end
--- spec/features/shelters/shelter_id_show_spec.rb
+++ spec/features/shelters/shelter_id_show_spec.rb
@@ -16,4 +16,24 @@ RSpec.describe "shelter id page", type: :feature do
     expect(page).to have_content(shelter_1.state)
     expect(page).to have_content(shelter_1.zip)
   end
+
+  it "can link to an edit page for the shelter id" do
+    shelter_1 = Shelter.create(name: "Denver Animal Shelter",
+                               address: "500 Invisible St.",
+                               city: "Denver",
+                               state: "Colorado",
+                               zip: "80201")
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_link("Update Shelter")
+
+    click_link("Update Shelter")
+
+    expect(page).to have_current_path("/shelters/#{shelter_1.id}/edit")
+    expect(page).to have_content('Name:')
+    expect(page).to have_content('Address:')
+    expect(page).to have_content('City:')
+    expect(page).to have_content('State:')
+    expect(page).to have_content('Zip:')
+  end
 end
